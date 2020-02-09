4e0437de5cf55d90092244c0514dcb26faf049b9
Shelly Schwartz <shellyganga@Shellys-Air.home>
2020-02-09 02:15:08 -0500

    forward algogit add .

--- HMM.py
+++ HMM.py
@@ -1,66 +1,70 @@
 #home use, pocket highway, hand highwat
 states = ('hu', 'hp', 'hh')
 
-#operational indicators
-o_i = ('meanSignalLenght', 'amplCor', 'syncVar', 'metr6.velSum')
-
-'''
-an observation is defined by the range that a value of an operational indicator falls in
-we are going to look at a window of 4 observations at a time, where each observation is a value of one of the 4 operational indicators
-struggle: getting the emission probilities 
-
-'''
-
-
-
-
-#how the heck do u pass a varible(integer) into a JSON object?? do we even need to do this?
 
 #no range, just numero -----for every oi
-raw_observation = {'meanSignalLength': val, ect}
+raw_observation = {'meanSignalLength': 1.0, 'amplCor': .97, 'syncVar' : 3, 'metr6.velSum': 1}
 
-#range-------for every oi
-new_observation = {'meanSignalLength': {'A', 'B', 'C'}, ect}
+#only looking at a sequence of two observations at a given period of time
+def updateObservation( meanSignalLength, amplCor, syncVar, velSum, raw_observation):
+    ob_s2 = []
+    ob_s2.append(raw_observation)
+    ob_s={}
+    ob_s['meanSignalLength'] = meanSignalLength
+    ob_s['amplCor'] = amplCor
+    ob_s['syncVar'] = syncVar
+    ob_s['metr6.velSum'] = velSum
+    ob_s2.append(ob_s)
+    ob_s_f = ob_s2
+    raw_observation = ob_s_f[1]
+    return ob_s_f
 
-'''
-i want to pass in a value for an observation into getObs 
-getObs is going to assign a range (A, B, C) to the operational indicator 
-where a = low range, b = middle range and c = high range
-the getObs methode will return the operational indicator with the correclty assigned range
-'''
-def getObs(o_i, val)
-    for i, oi in enumerate(o_i):
-        if(oi=='meanSignalLength')
-            if(val is in lower range)
-                range = 'A'
-                return new_observation[o_i][range]
+def getRange(observations):
+    range = ''
+    for observation in observations:
+            if observation == 'meanSignalLength':
+                if raw_observation[observation] in range(.078,.48):
+                    range = 'A'
+                if raw_observation[observation] in range(.478, .880):
+                    range = 'B'
+                if raw_observation[observation] in range(.878, 1.480):
+                    range = 'C'
+            if observation == 'amplCor':
+                if raw_observation[observation] in range(.104, .466):
+                    range = 'A'
+                if raw_observation[observation] in range(.464, .735):
+                    range = 'B'
+                if raw_observation[observation] in range(.735, .975):
+                    range = 'C'
+    return range
 
 
-int_prob = {
+inti_prob = {
     'hu' : 0.5,
     'hp' : 0.3,
     'hh' : 0.2
 }
 
-trans_prob = {
+transi_prob = {
     'hu': { 'hu' : 0.8, 'hh' : 0.1, 'hp' : 0.1 },
-    'hh': { 'hu' : 0.15, 'hh' : 0.8, 'hp' : 0.05 },
-    'hp': { 'hu' : 0.25, 'hh' : 0.25, 'hp' : 0.05}
+    'hp': { 'hu' : 0.15, 'hh' : 0.8, 'hp' : 0.05 },
+    'hh': { 'hu' : 0.25, 'hh' : 0.25, 'hp' : 0.05}
 }
 
 # given the state, what is the probility of a certain observation?
+#A=low, b= med, c= high
 emit_prob_indv = {
-    'hu' : {'meanSignalLength' : { 'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
+    'hu' : {'meanSignalLength' : { 'A' : 0.6195, 'B' : 0.3066, 'C' : 0.076},
             'amplCor' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
             'syncVar' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
             'metr6.velSum' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2}
     },
-    'hh' : {'meanSignalLength' : { 'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
+    'hp' : {'meanSignalLength' : {'A' : 0.5682, 'B' : 0.5, 'C' : 0.2 },
             'amplCor' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
             'syncVar' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
             'metr6.velSum' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2}
     },
-    'hh' : {'meanSignalLength' : { 'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
+    'hh' : {'meanSignalLength' : { 'A' : 0.8247, 'B' : 0.1635, 'C' : 0.0382},
             'amplCor' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
             'syncVar' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2},
             'metr6.velSum' : {'A' : 0.3, 'B' : 0.5, 'C' : 0.2}
@@ -68,28 +72,36 @@ emit_prob_indv = {
 }
 
 
+
 def int_prob(state):
-    return int_prob[state]
+    return inti_prob[state]
 
 def trans_prob(prevstate, state):
-    return trans_prob[prevstate,state]
+    return transi_prob[prevstate][state]
 
 
-def emit_prob(state, getObs(o_i, val))
-   '''
-   use getObs to find the "new observation"
-   for each state find the emiision probility of the new observation and then multiply the new observations
-   asojsadfhdf i am probly over complicating this its 5 am good night
-   '''
+def emit_prob(state, raw_observations, rangE):
+    value = 1
+    for observation in raw_observations:
+        value *= emit_prob_indv[state][observation][rangE]
+    return value
 
 
-def forward(states, observations):
-    alpha=[{}]
+def forward(sequence):
+    sequence_length = len(sequence)
+    alpha = [{}]
     for state in states:
-        alpha[0][state]=int_prob(state)*emit_prob()
-    for observation in observations:
-        for state in states:
-            #observation - 1 --> previous observation, same thing for state
-           alpha[observation][state] += alpha[observation-1][state-1] * trans_prob(state-1, state) * emit_prob()
-    alpha_prob = alpha[observation][state]
-return alpha_prob
+        alpha[0][state] = int_prob(state) * emit_prob(state, sequence[0], getRange(sequence[0]))
+    for index in range(1, sequence_length):
+        alpha.append({})
+        for state_to in states:
+            prob = 0
+            for state_from in states:
+                prob += alpha[index - 1][state_from] * trans_prob(state_from, state_to)
+            alpha[index][state_to] = prob * emit_prob(state_to, sequence[index], getRange(sequence[0]))
+    return alpha[1]
+
+
+sequenceOfOb = updateObservation(1,1,1,1,raw_observation)
+print(sequenceOfOb)
+print(forward(sequenceOfOb))
